d54138e9bb4861d6785861e54642652fa7003c08
taylorchu <tailinchu@gmail.com>
2015-04-12 09:09:06 -0700

    init

---
+++ router.go
@@ -0,0 +1,30 @@
+package mux
+
+import (
+	"github.com/go-ndn/lpm"
+	"github.com/go-ndn/ndn"
+)
+
+type Handler interface {
+	ServeNDN(*ndn.Face, *ndn.Interest)
+}
+
+type Router struct {
+	m lpm.Matcher
+}
+
+func New() *Router {
+	return &Router{m: lpm.New()}
+}
+
+func (r *Router) Handle(name string, h Handler) {
+	r.m.Update(name, func(v interface{}) interface{} { return h }, false)
+}
+
+func (r *Router) Run(face *ndn.Face, ch <-chan *ndn.Interest) {
+	for i := range ch {
+		r.m.Match(i.Name.String(), func(v interface{}) {
+			go v.(Handler).ServeNDN(face, i)
+		})
+	}
+}
