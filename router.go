20b25b8a58f929b865904f8d0196798dc0f03582
taylorchu <tailinchu@gmail.com>
2015-04-12 14:36:47 -0700

    add handlerfunc

--- router.go
+++ router.go
@@ -5,7 +5,15 @@ import (
 	"github.com/go-ndn/ndn"
 )
 
-type Handler func(*ndn.Face, *ndn.Interest)
+type HandlerFunc func(*ndn.Face, *ndn.Interest)
+
+func (f HandlerFunc) ServeNDN(face *ndn.Face, i *ndn.Interest) {
+	f(face, i)
+}
+
+type Handler interface {
+	ServeNDN(*ndn.Face, *ndn.Interest)
+}
 
 type Router struct {
 	m lpm.Matcher
@@ -19,10 +27,18 @@ func (r *Router) Handle(name string, h Handler) {
 	r.m.Update(name, func(v interface{}) interface{} { return h }, false)
 }
 
+func (r *Router) HandleFunc(name string, h HandlerFunc) {
+	r.m.Update(name, func(v interface{}) interface{} { return h }, false)
+}
+
+func (r *Router) ServeNDN(face *ndn.Face, i *ndn.Interest) {
+	r.m.Match(i.Name.String(), func(v interface{}) {
+		v.(Handler).ServeNDN(face, i)
+	})
+}
+
 func (r *Router) Run(face *ndn.Face, ch <-chan *ndn.Interest) {
 	for i := range ch {
-		r.m.Match(i.Name.String(), func(v interface{}) {
-			go v.(Handler)(face, i)
-		})
+		go r.ServeNDN(face, i)
 	}
 }
