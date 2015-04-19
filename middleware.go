c0f3bf948b551e86f7ebf6d5cbb5fb8137c4a302
taylorchu <tailinchu@gmail.com>
2015-04-18 19:07:22 -0700

    refactor middleware

--- middleware.go
+++ middleware.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/go-ndn/ndn"
@@ -32,27 +31,20 @@ func (c *Chain) Run(w DataSender, ch <-chan *ndn.Interest) {
 	}
 }
 
-type dataSender struct {
-	mu      sync.Mutex
-	content []*ndn.Data
+type cacher struct {
+	w DataSender
 }
 
-func (s *dataSender) SendData(d *ndn.Data) {
-	s.mu.Lock()
-	s.content = append(s.content, d)
-	s.mu.Unlock()
+func (c *cacher) SendData(d *ndn.Data) {
+	c.w.SendData(d)
+	ndn.ContentStore.Add(d)
 }
 
 func Cacher(next Handler) Handler {
 	return HandlerFunc(func(w DataSender, i *ndn.Interest) {
 		cache := ndn.ContentStore.Get(i)
 		if cache == nil {
-			s := new(dataSender)
-			next.ServeNDN(s, i)
-			for _, d := range s.content {
-				w.SendData(d)
-				ndn.ContentStore.Add(d)
-			}
+			next.ServeNDN(&cacher{w: w}, i)
 		} else {
 			w.SendData(cache)
 		}
@@ -67,37 +59,42 @@ func Logger(next Handler) Handler {
 	})
 }
 
+type segmentor struct {
+	w    DataSender
+	size int
+}
+
+func (s *segmentor) SendData(d *ndn.Data) {
+	if len(d.Content) <= s.size {
+		s.w.SendData(d)
+	} else {
+		segNum := make([]byte, 8)
+		for start := 0; start < len(d.Content); start += s.size {
+			end := start + s.size
+			if end > len(d.Content) {
+				end = len(d.Content)
+			}
+			binary.BigEndian.PutUint64(segNum, uint64(start))
+			seg := &ndn.Data{
+				Name: ndn.Name{Components: append(d.Name.Components, segNum)},
+				MetaInfo: ndn.MetaInfo{
+					ContentType:     d.MetaInfo.ContentType,
+					FreshnessPeriod: d.MetaInfo.FreshnessPeriod,
+				},
+				Content: d.Content[start:end],
+			}
+			if len(seg.Content) < s.size {
+				seg.MetaInfo.FinalBlockID.Component = segNum
+			}
+			s.w.SendData(seg)
+		}
+	}
+}
+
 func Segmentor(size int) Middleware {
 	return func(next Handler) Handler {
 		return HandlerFunc(func(w DataSender, i *ndn.Interest) {
-			s := new(dataSender)
-			next.ServeNDN(s, i)
-			for _, d := range s.content {
-				if len(d.Content) <= size {
-					w.SendData(d)
-					continue
-				}
-				for start := 0; start < len(d.Content); start += size {
-					end := start + size
-					if end > len(d.Content) {
-						end = len(d.Content)
-					}
-					segNum := make([]byte, 8)
-					binary.BigEndian.PutUint64(segNum, uint64(start))
-					seg := &ndn.Data{
-						Name: ndn.Name{Components: append(d.Name.Components, segNum)},
-						MetaInfo: ndn.MetaInfo{
-							ContentType:     d.MetaInfo.ContentType,
-							FreshnessPeriod: d.MetaInfo.FreshnessPeriod,
-						},
-						Content: d.Content[start:end],
-					}
-					if len(seg.Content) < size {
-						seg.MetaInfo.FinalBlockID.Component = segNum
-					}
-					w.SendData(seg)
-				}
-			}
+			next.ServeNDN(&segmentor{w: w, size: size}, i)
 		})
 	}
 }
